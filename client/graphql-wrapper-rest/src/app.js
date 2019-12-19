18c67a6de8dc401833fbf321f95656f2e56776e8
Kartik Malhotra <kartik@kartiks-mbp-2.home>
2019-12-19 17:06:53 +0000

    Removed the uri from client

--- client/graphql-wrapper-rest/src/app.js
+++ client/graphql-wrapper-rest/src/app.js
@@ -25,11 +25,12 @@ const initApollo = () => {
   });
 
   const client = new ApolloClient({
-    link: new HttpLink({ uri: 'http://localhost:4000/', fetch: fetch }),
+    //link: new HttpLink({ uri: 'http://localhost:4000/', fetch: fetch }),
+    link: new HttpLink({ fetch: fetch }),
     //link: new SchemaLink({server}),
     cache: new InMemoryCache(),
   });
-
+  console.dir(HttpLink);
   return client;
 };
 
